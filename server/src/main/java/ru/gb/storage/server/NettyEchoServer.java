235edc75d540b646f3c828e08cb70b0b8aa52d1a
cmexye4ek <89582141+cmexye4ek@users.noreply.github.com>
2022-04-11 14:20:32 +0500

    done

--- server/src/main/java/ru/gb/storage/server/NettyEchoServer.java
+++ server/src/main/java/ru/gb/storage/server/NettyEchoServer.java
@@ -29,8 +29,6 @@ public class NettyEchoServer {
     }
 
     public void start() throws InterruptedException {
-        ArrayList<String> temp = new ArrayList<>();
-        final StringBuilder sb = new StringBuilder();
         NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
@@ -42,54 +40,7 @@ public class NettyEchoServer {
                         @Override
                         protected void initChannel(NioSocketChannel ch) {
                             ch.pipeline().addLast(
-                                    new ChannelInboundHandlerAdapter() {
-                                        @Override
-                                        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("Channel registered");
-                                        }
-
-                                        @Override
-                                        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("Channel unregistered");
-                                        }
-
-                                        @Override
-                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("Channel active");
-                                        }
-
-                                        @Override
-                                        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("Channel inactive");
-                                        }
-
-                                        @Override
-                                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-                                            System.out.println("Cause exception");
-                                            System.out.println(cause);
-                                            ctx.close();
-                                        }
-
-                                        @Override
-                                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                            final ByteBuf m = (ByteBuf) msg;
-                                            if (!(new String(ByteBufUtil.getBytes(m)).contains(System.lineSeparator()))) {
-                                                System.out.println("Channel read");
-                                                temp.add(new String(ByteBufUtil.getBytes(m)));
-                                                System.out.println(new String(ByteBufUtil.getBytes(m)));
-                                            } else {
-                                                for (String s : temp) {
-                                                    sb.append(s);
-                                                }
-                                                temp.clear();
-                                                System.out.println("New message: " + sb);
-                                                msg = m.writeBytes(("ECHO: " + sb + System.lineSeparator() + System.lineSeparator()).getBytes());
-                                                ctx.writeAndFlush(msg);
-                                                System.out.println("Echo send");
-                                                sb.setLength(0);
-                                            }
-                                        }
-                                    }
+                                    new NettyEchoServerHandler()
                             );
                         }
                     })
